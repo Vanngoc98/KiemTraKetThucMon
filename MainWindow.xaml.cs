de2d32da5233b78861ce4be58107f65662b77d98
Dark <56054516+Vanngoc98@users.noreply.github.com>
2019-10-03 15:21:41 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kiemtraketthucmon2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnmeo_Click(object sender, RoutedEventArgs e)
+        {
+            meo meo = new meo();
+            meo.tiengkeu = "Meo meo";
+            meo.maulong = "Vàng";
+            meo.Talk();
+        }
+
+        private void Btncho_Click(object sender, RoutedEventArgs e)
+        {
+            cho cho = new cho();
+            cho.tiengkeu = "Gâu gâu";
+            cho.maulong = "Đen";
+            cho.Talk();
+        }
+    }
+}
